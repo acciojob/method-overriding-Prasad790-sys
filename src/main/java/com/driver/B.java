952ebe9f354bc810fb22e364b135f07d69b6b5a6
Prasad <psa19974@gmail.com>
2024-07-16 22:16:15 +0530

    Push

---
+++ src/main/java/com/driver/B.java
@@ -0,0 +1,10 @@
+package com.driver;
+
+public class B {
+    B(){
+        super.getClass();
+    }
+    public String meth(){
+        return "Method is overridden in Extendend class B";
+    }
+}
